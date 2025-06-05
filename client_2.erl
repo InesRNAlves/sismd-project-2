68104221c4394f47b6647677668f9869ad70a2fc
AlvesI1-ppb <ines.alves@flutter.com>
2025-06-05 23:02:02 +0100

    Core features + Data storage --> functional client and Server + Failure Detection.

--- client_2.erl
+++ client_2.erl
@@ -9,19 +9,14 @@
 
 % Starts a new process with name 'Client'
 start(Client, Neighbors, MessageIntervalMs) ->
-  Pid = spawn_link(fun() -> init(Client, Neighbors, MessageIntervalMs) end),
+  Pid = spawn(fun() -> init(Client, Neighbors, MessageIntervalMs) end),
   register(Client, Pid).
 
 % Sends a stop message to the 'Client' process to shut it down
 stop(Client) ->
   Client ! stop_process.
 
-% I Want to Handle Errors If a Process I Create Crashes --> process_flag(trap_exit, true).
-% When trap_exit is set to true, exit signals arriving to a process are
-% converted to {'EXIT', From, Reason} messages, which can be received as ordinary messages
 init(Client, Neighbors, MessageIntervalMs) ->
-  process_flag(trap_exit, true),
-  rand:seed(exsplus, erlang:timestamp()), % or rand:seed(exsplus, {erlang:monotonic_time(), erlang:unique_integer(), node()}),
   State = #state{client = Client, neighbors = Neighbors, message_interval_ms = MessageIntervalMs},
   io:format("Client ~p started with neighbors: ~p~n", [Client, Neighbors]),
   loop(State).
@@ -34,10 +29,16 @@ loop(State = #state{client = Client, message_interval_ms = MessageIntervalMs, ne
       [_Failed | Remaining] = Neighbors,
       io:format("Client ~p: Neighbor down, remaining neighbors: ~p~n", [Client, Remaining]),
       loop(State#state{neighbors = Remaining});
-    {'EXIT', Pid, Reason} ->
-      Freq = find_pid(Pid,Frequencies),
-      NewFrequencies = deallocate(Frequencies, Freq),
-loop(NewFrequencies)
+    {From, {store, Key, Value}} ->
+      Reply = server:store(Key, Value),
+      From ! {self(), Reply},
+      io:format("Client ~p (relay): forwarded ~p = ~p to server~n", [Client, Key, Value]),
+      loop(State)
+
+  % After the defined interval, it: Picks a key like "temperature"
+  % Generates a value (e.g., 27)
+  % Tries to send to a neighbor
+  % Recursively loops
   after MessageIntervalMs ->
     Key = random_key(),
     Value = generate_value(Key),
@@ -45,36 +46,53 @@ loop(NewFrequencies)
     loop(State)
   end.
 
-try_neighbors([], Key, _Value, State) ->
-  io:format("Client ~p: No available neighbors for key ~p~n", [State#state.client, Key]);
+% Tries to send to the first available neighbor
+% If it fails, it will call itself recursively with the remaining neighbors
+% If no neighbors are available, it logs a message
+try_neighbors([], Key, Value, State) ->
+  io:format("Client '~p': No available neighbors to send ~p = ~p~n", [State#state.client, Key, Value]);
 try_neighbors([{Client, Node} | Rest], Key, Value, State) ->
   try_send(Client, Node, Key, Value, Rest, State).
 
+% Gets the PID of the remote process on Node with name Client
+% If the PID is not found, Pid = undefined in the try_monitor/7 function
 try_send(Client, Node, Key, Value, Rest, State) ->
   Pid = rpc:call(Node, erlang, whereis, [Client]),
   try_monitor(Pid, Client, Node, Key, Value, Rest, State).
 
+% If the PID is undefined, tries the next neighbor
 try_monitor(undefined, Client, Node, Key, Value, Rest, State) ->
   io:format("Client ~p: ~p@~p unreachable, trying next~n", [State#state.client, Client, Node]),
   try_neighbors(Rest, Key, Value, State);
+
+% If the PID is found, sends the message
 try_monitor(Pid, Name, Node, Key, Value, Rest, State) ->
+  % Monitor the process to catch crashes
   Ref = monitor(process, Pid),
+  % Send the message to the remote process (PID) with reference to self,
+  % so that the receiving process can reply
   Pid ! {self(), {store, Key, Value}},
   receive
+    % Remove the monitor reference and flush any 'DOWN' messages from the mailbox,
+    % in case the process crashed while waiting for the reply
     {_, Reply} ->
       demonitor(Ref, [flush]),
       io:format("Client ~p: Sent ~p = ~p to ~p@~p, reply: ~p~n",
         [State#state.client, Key, Value, Name, Node, Reply])
+
+  % If the process does not respond within 1s, it is considered unreachable and tries the next neighbor
   after 1000 ->
     demonitor(Ref, [flush]),
     io:format("Client ~p: ~p@~p timeout, trying next~n", [State#state.client, Name, Node]),
     try_neighbors(Rest, Key, Value, State)
   end.
 
+% Picks a random key from the list of keys
 random_key() ->
   Keys = ["humidity", "temperature", "co2_level", "pressure"],
   lists:nth(rand:uniform(length(Keys)), Keys).
 
+% Generates a random value based on the key
 generate_value("humidity") -> rand:uniform(100);
 generate_value("temperature") -> rand:uniform(35) + 5;
 generate_value("co2_level") -> rand:uniform(800) + 400;
--- server.erl
+++ server.erl
@@ -7,33 +7,30 @@ stop() ->
   central ! stop_server.
 
 store(Key, Value) -> rpc({store, Key, Value}).
+
 lookup(Key) -> rpc({lookup, Key}).
-rpc(Q) ->
-  central ! {self(), Q},
+
+rpc(Query) ->
+  central ! {self(), Query},
   receive
     {central, Reply} -> Reply
   end.
 
 loops(Store) ->
   receive
-
     stop_server ->
-      io:format("CENTRAL SERVER: Shutting down~n"),
-      ok;
+      io:format("Central Server: Shutting down~n"), ok;
 
     {From, {store, Key, Value}} ->
       % Append the value to the list for that key
-      UpdatedStore = maps:update_with(Key,
-        fun(OldList) -> [Value | OldList] end,
-        [Value],
-        Store),
+      UpdatedStore = maps:update_with(Key, fun(OldList) -> [Value | OldList] end, [Value], Store),
       From ! {central, {ok, Value}},
-      io:format("CENTRAL STORE: ~p -> ~p = ~p~n", [node(From), Key, Value]),
+      io:format("Central Server STORE: ~p -> ~p = ~p~n", [node(From), Key, Value]),
       loops(UpdatedStore);
 
     {From, {lookup, Key}} ->
       Value = Value = lists:reverse(maps:get(Key, Store, [])), % orders the values by the order in which they were received
       From ! {central, Value},
-      io:format("CENTRAL LOOKUP: ~p requested ~p -> ~p~n", [node(From), Key, Value]),
+      io:format("Central Server LOOKUP: ~p requested ~p -> ~p~n", [node(From), Key, Value]),
       loops(Store)
   end.
